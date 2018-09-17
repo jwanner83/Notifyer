6cb94dbefbea9b7f2a8688ed193a9f93b2aa7ce7
Jonas Wanner <jonas.wanner@clavisit.com>
2018-09-17 17:41:29 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 // Setup basic express server
-var express = require('express');
-var app = express();
-var path = require('path');
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
+var express = require('express')
+var app = express()
+var path = require('path')
+var server = require('http').createServer(app)
+var io = require('socket.io')(server)
+var port = process.env.PORT || 3000
 
 server.listen(port, function(){
-    console.log('Erreichbar ufem Port ' + port);
+    console.log('Erreichbar ufem Port ' + port)
 });
 
 // Routing
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Chatroom
 io.on('connection', function(socket){
     // when the client emits 'chat message', this listens and executes
     socket.on('request help', function(msg, role, time, room){
         console.log("help is requested: " + msg + ', ' + role + ', ' + time + ', ' + room)
-        io.emit('request help', msg, role, time, room);
-    });
-});
+        io.emit('request help', msg, role, time, room)
+    })
+})
